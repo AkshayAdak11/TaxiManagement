7c9ecb0b6d1940d82e5799db28623648753c34dc
akshaykumar.adak <akshaykumar.adak@phonepe.com>
2024-08-25 14:05:25 +0530

    added current location of taxi

--- src/main/java/com/taxifleet/cronjob/BookingProcessor.java
+++ src/main/java/com/taxifleet/cronjob/BookingProcessor.java
@@ -5,40 +5,47 @@ import com.taxifleet.services.BookingService;
 import com.taxifleet.services.DashboardService;
 
 import java.util.List;
+import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
 public class BookingProcessor {
     private final BookingService bookingService;
-    private final ScheduledExecutorService executorService;
-
+    private final ScheduledExecutorService schedulerService;
+    private final ExecutorService pendingWorkerService;
     private final DashboardService dashboardService;
 
     public BookingProcessor(BookingService bookingService,
                             DashboardService dashboardService) {
         this.bookingService = bookingService;
         this.dashboardService = dashboardService;
-        this.executorService = Executors.newScheduledThreadPool(20);
+        this.schedulerService = Executors.newScheduledThreadPool(10); //We have kept the scheduler thread separate
+        this.pendingWorkerService = Executors.newFixedThreadPool(20); // We have kept pending worker thread separate
     }
 
     public void startProcessing() {
-        executorService.scheduleAtFixedRate(() -> {
+        schedulerService.scheduleAtFixedRate(() -> {
             List<StoredBooking> pendingBookings = bookingService.allPendingBooking();
             for (StoredBooking booking : pendingBookings) {
-                executorService.submit(new BookingTask(bookingService, booking, dashboardService));
+                pendingWorkerService.submit(new BookingTask(bookingService, booking, dashboardService));
             }
         }, 0, 1, TimeUnit.MINUTES);
     }
 
     public void shutdown() {
-        executorService.shutdown();
+        schedulerService.shutdown();
+        pendingWorkerService.shutdown();
         try {
-            if (!executorService.awaitTermination(60, TimeUnit.SECONDS)) {
-                executorService.shutdownNow();
+            if (!schedulerService.awaitTermination(60, TimeUnit.SECONDS)) {
+                schedulerService.shutdownNow();
+            }
+            if (!pendingWorkerService.awaitTermination(60, TimeUnit.SECONDS)) {
+                pendingWorkerService.shutdownNow();
             }
         } catch (InterruptedException e) {
-            executorService.shutdownNow();
+            schedulerService.shutdownNow();
+            pendingWorkerService.shutdownNow();
         }
     }
 }
